7372caf3a3ae35126c0149d13a77253036e63fce
alwi9710 <93672175+alwi9710@users.noreply.github.com>
2023-03-07 11:28:13 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,10 @@
 from guizero import App, Text, error, TextBox, PushButtonm, Picture, info, ButtonGroup, Window
 passed = False 
 app = App(title = 'ROBO', width = 194*5, height = 110*5)
-app.bg = 'yellow'
+app.bg = '#FF5F15'
+app.text_size = 70
+app.font = 'Helvetica'
+app.text_color = '#2073B3'
 
 def authenticate():
   if password.value == 'password':
@@ -48,8 +51,10 @@ def main_menu_hide()
 menu_header = Text(app, text = 'menu')
 menu_header.hide()
 Send_A_B = PushButton(app, command = send_A_B, text = 'Send on A to B')
+Send_A_B.bg = '#0894FF'
 Send_A_B.hide()
 Show_Stats = PushButton(app, command = show_stats, text = 'Show Stats')
+Show_Stats.bg = '#0894FF'
 Show_Stats.hide()
 route_message = Text(app, text = 'ON ROUTE FROM A TO B')
 route_message.hide()
@@ -58,11 +63,13 @@ stats.hide()
 shut_down_message = Text(app, text = 'Shutting down...')
 shut_down_message.hide()
 back_to_menu = PushButton(app, text = 'Back to Main Menu', command = main_menu)
+back_to_menu.bg = '#0894FF'
 back_to_menu.hide()
 
 message = Text(app, text = 'surprise SHAWTY', size = 30)
 password = TextBox(app, hide_text = True, visible = True, width = 20)
 submit = PushButton(app, enabled = True, command = authenticate, text = 'submit', height = 1, width = 6)
+submit.bg = '#0894FF'
 
 app.display()
   
